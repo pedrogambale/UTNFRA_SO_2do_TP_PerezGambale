43d5a6b4b8d45cf86af3b6743064b1a4e0405f0c
pedrogambale <pedroperezgambale.utn@gmail.com>
2026-06-23 06:46:30 -0300

    Punto_C.sh

--- RTA_Examen_20260623/Punto_C.sh
+++ RTA_Examen_20260623/Punto_C.sh
@@ -1,11 +1,3 @@
-cat << 'EOF' > ~/UTN-FRA_SO_Examenes/202406/docker/run.sh
-#!/bin/bash
-docker run -d -p 8080:80 pedrogambale/web1-perezgambale:latest
-EOF
-
-chmod +x ~/UTN-FRA_SO_Examenes/202406/docker/run.sh
-~/UTN-FRA_SO_Examenes/202406/docker/run.sh
-sudo ~/UTN-FRA_SO_Examenes/202406/docker/run.sh
 cat << 'EOF' > ~/RTAExamen$(date +%Y%m%d)/Punto_C.sh
 #!/bin/bash
 cd ~/UTN-FRA_SO_Examenes/202406/docker/
