18d258818ba8d550a21091c757d27d86ff5ccf0b
pedrogambale <pedroperezgambale.utn@gmail.com>
2026-06-23 06:47:27 -0300

    Punto_B.sh

--- RTA_Examen_20260623/Punto_B.sh
+++ RTA_Examen_20260623/Punto_B.sh
@@ -1,20 +1,4 @@
-sudo chmod +x /usr/local/bin/PerezGambaleAltaUser-Groups.sh
-sudo /usr/local/bin/PerezGambaleAltaUser-Groups.sh vagrant $LISTA_PATH
-tail -n 10 /etc/passwd
-cd ~/UTN-FRA_SO_Examenes/202406/docker/
-vim index.html
-history
-vim index.html
-cd ~/UTN-FRA_SO_Examenes/202406/docker/
-cd ~/UTN-FRA_SO_Examenes/202406/docker/
-vim index.html
-docker build -t pedrogambale/web1-perezgambale:latest .
-sudo usermod -a -G docker vagrant
-cat << 'EOF' > Dockerfile
-FROM nginx
-COPY index.html /usr/share/nginx/html/index.html
+cat << 'EOF' > ~/UTN-FRA_SO_Examenes/202406/docker/run.sh
+#!/bin/bash
+docker run -d -p 8080:80 pedrogambale/web1-perezgambale:latest
 EOF
-
-sudo docker build -t pedrogambale/web1-perezgambale:latest
-sudo docker build -t pedrogambale/web1-perezgambale:latest .
-sudo docker push pedrogambale/web1-perezgambale:latest
